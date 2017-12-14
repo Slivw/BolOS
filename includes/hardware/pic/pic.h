45105c350b158355c5bc7e1a597e24f902c4b230
Romain Allée <romain.allee@epitech.eu>
2017-12-14 16:56:18 +0100

    add pic initialisation

---
+++ includes/hardware/pic/pic.h
@@ -0,0 +1,26 @@
+#ifndef PIC_H_
+#define PIC_H_
+
+#define PIC1		0x20
+#define PIC2		0xA0
+
+#define PIC1_DATA	0x21
+#define PIC2_DATA	0xA1
+
+#define PIC_EOI		0x20
+
+#define ICW1_ICW4	0x01
+#define ICW1_INIT	0x10
+ 
+#define ICW4_8086	0x01
+
+#define PIC_1_OFFSET 0X20
+#define PIC_2_OFFSET 0X28
+
+#define PIC1_EOF_INT outb(PIC1,PIC_EOI)
+ 
+#define PIC2_EOF_INT outb(PIC2,PIC_EOI)
+
+void init_pic();
+
+#endif
--- srcs/boot/kmain.c
+++ srcs/boot/kmain.c
@@ -1,6 +1,8 @@
 #include "boot/boot.h"
+#include "hardware/pic/pic.h"
 
 void bos_kmain(s_boot_info* p_bif)
 {
+    init_pic();
     while (1);
 }
